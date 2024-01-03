15c7538f395160df23f240687c9c24a2ed1ed0cb
Norhan Waleed <105253730+Norhan-salem@users.noreply.github.com>
2024-01-04 01:34:16 +0200

    Update CliEditor.py

--- CliEditor.py
+++ CliEditor.py
@@ -1,6 +1,36 @@
-import threading
+"""
+    ## Extending Eran Ulas' Chatting Application
+    ## Team 24
+"""
+
 from colorama import Fore, Style, init
 from pyfiglet import Figlet
+from rich import print
+import re
+
+def activate_link(messageReceived):
+    # Regular expression to match URLs
+    url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    urls = re.findall(url_pattern, messageReceived)
+
+    # Replace each URL with a markdown link
+    for url in urls:
+        message = messageReceived.replace(url, f'{url}')
+
+    return messageReceived
+
+
+def format_message(messageReceived):
+    if messageReceived.endswith("/B"):
+        # Remove the last two characters "/B" and make the messageReceived bold
+        print("[bold]" + messageReceived[:-2] + "[/bold]")
+    elif messageReceived.endswith("/I"):
+        # Remove the last two characters "/I" and make the messageReceived italic
+        print("[italic]" + messageReceived[:-2] + "[/italic]")
+    else:
+        # If the messageReceived doesn't end with "/B" or "/I", print it as is
+        print(messageReceived)
+
 
 # Initialize colorama
 init()
@@ -35,5 +65,18 @@ def yellow_message(message, color=Fore.LIGHTYELLOW_EX, style=Style.RESET_ALL):
 def blue_message(message, color=Fore.LIGHTBLUE_EX, style=Style.RESET_ALL):
     print(f"{style}{color}{message}{Style.RESET_ALL}", end='\n')
 
+
 def green_message_without_space(message, color=Fore.LIGHTGREEN_EX, style=Style.RESET_ALL):
+    return f"{style}{color}{message}{Style.RESET_ALL}"
+
+
+def yellow_message_without_space(message, color=Fore.LIGHTYELLOW_EX, style=Style.RESET_ALL):
+    return f"{style}{color}{message}{Style.RESET_ALL}"
+
+
+def red_message_without_space(message, color=Fore.LIGHTRED_EX, style=Style.RESET_ALL):
+    return f"{style}{color}{message}{Style.RESET_ALL}"
+
+
+def blue_message_without_space(message, color=Fore.LIGHTBLUE_EX, style=Style.RESET_ALL):
     return f"{style}{color}{message}{Style.RESET_ALL}"
